b3041bf6d1bc8a2d54588d9fa544b24b58a005c9
mr.Shu <mr@shu.io>
2024-08-05 08:20:20 +0200

    install: Fix fd, refactor
    * Fix fd on macOS
    * Run through with black
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
-from pyinfra.operations import (brew, dnf)
+from pyinfra.operations import brew, dnf
 from pyinfra import facts
 from pyinfra import host
 
@@ -21,34 +21,36 @@ class Package:
 
 
 PACKAGES = (
-    Package('alacritty'),
-    Package('bat'),
-    Package('direnv'),
-    Package('fd-find'),
-    Package('fish'),
-    Package('flameshot'),
-    Package('gh'),
-    Package(None, dnf_package='gnome-pomodoro'),
+    Package("alacritty"),
+    Package("bat"),
+    Package("direnv"),
+    Package(None, dnf_package="fd-find", brew_package="fd"),
+    Package("fish"),
+    Package("flameshot"),
+    Package("gh"),
+    Package(None, dnf_package="gnome-pomodoro"),
     # https://fedora.pkgs.org/37/terra/hack-nerd-fonts-0:2.3.3-2.fc37.noarch.rpm.html
     # dnf config-manager --add-repo https://terra.fyralabs.com/terra.repo
-    Package(None,
-            dnf_package='hack-nerd-fonts',
-            brew_package='homebrew/cask-fonts/font-hack-nerd-font'),
-    Package('htop'),
-    Package('jq'),
+    Package(
+        None,
+        dnf_package="hack-nerd-fonts",
+        brew_package="homebrew/cask-fonts/font-hack-nerd-font",
+    ),
+    Package("htop"),
+    Package("jq"),
     # https://copr.fedorainfracloud.org/coprs/atim/lazygit/
     # dnf copr enable atim/lazygit -y
-    Package('lazygit'),
-    Package('neovim'),
-    Package('ncdu'),
-    Package('ripgrep'),
-    Package(None, brew_package='rectangle'),
+    Package("lazygit"),
+    Package("neovim"),
+    Package("ncdu"),
+    Package("ripgrep"),
+    Package(None, brew_package="rectangle"),
     # https://copr.fedorainfracloud.org/coprs/atim/starship/
     # dnf copr enable atim/starship
-    Package('starship'),
-    Package('task'),
-    Package('tmux'),
-    Package('zellij'),
+    Package("starship"),
+    Package("task"),
+    Package("tmux"),
+    Package("zellij"),
 )
 
 
@@ -76,21 +78,21 @@ def get_packages_for_platform(packages: tuple, package_manager: str) -> list:
 def main():
     platform = get_os_platform()
 
-    if platform == 'darwin':
-        packages = get_packages_for_platform(PACKAGES, 'brew')
+    if platform == "darwin":
+        packages = get_packages_for_platform(PACKAGES, "brew")
         brew.packages(
             name=f"Install packages {', '.join(packages)}",
             packages=packages,
-            update=True
+            update=True,
         )
 
-    elif platform == 'linux':
-        packages = get_packages_for_platform(PACKAGES, 'dnf')
+    elif platform == "linux":
+        packages = get_packages_for_platform(PACKAGES, "dnf")
         dnf.packages(
             name=f"Install packages {', '.join(packages)}",
             packages=packages,
             latest=True,
-            _sudo=True
+            _sudo=True,
         )
 
 
