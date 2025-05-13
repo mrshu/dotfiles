9ce7517749fb64f2fbe923d113187616f9177d7d
mr.Shu <mr@shu.io>
2025-05-13 20:34:40 +0200

    add: Bump `lazyvim` dependencies
    * Bump some of the dependencies of `lazyvim`.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- dot_config/nvim/lua/plugins/starters.lua
+++ dot_config/nvim/lua/plugins/starters.lua
@@ -1,27 +1,23 @@
 return {
   ---- change trouble config
-  --{
-  --  "folke/trouble.nvim",
-  --  -- opts will be merged with the parent spec
-  --  opts = { use_diagnostic_signs = true },
-  --},
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
   --
   ---- disable trouble
   --{ "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+  --{
+  --  "hrsh7th/nvim-cmp",
+  --  dependencies = { "hrsh7th/cmp-emoji" },
+  --  ---@param opts cmp.ConfigSchema
+  --  opts = function(_, opts)
+  --    table.insert(opts.sources, { name = "emoji" })
+  --  end,
+  --},
 
   -- add more treesitter parsers
   {
@@ -60,9 +56,6 @@ return {
     end,
   },
 
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
@@ -72,46 +65,63 @@ return {
     end,
   },
   -- then: setup supertab in cmp
+  --{
+  --  "hrsh7th/nvim-cmp",
+  --  dependencies = {
+  --    "hrsh7th/cmp-emoji",
+  --  },
+  --  ---@param opts cmp.ConfigSchema
+  --  opts = function(_, opts)
+  --    local has_words_before = function()
+  --      unpack = unpack or table.unpack
+  --      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --    end
+  --
+  --    local luasnip = require("luasnip")
+  --    local cmp = require("cmp")
+  --
+  --    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --      ["<Tab>"] = cmp.mapping(function(fallback)
+  --        if cmp.visible() then
+  --          cmp.select_next_item()
+  --          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+  --          -- this way you will only jump inside the snippet region
+  --        elseif luasnip.expand_or_jumpable() then
+  --          luasnip.expand_or_jump()
+  --        elseif has_words_before() then
+  --          cmp.complete()
+  --        else
+  --          fallback()
+  --        end
+  --      end, { "i", "s" }),
+  --      ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --        if cmp.visible() then
+  --          cmp.select_prev_item()
+  --        elseif luasnip.jumpable(-1) then
+  --          luasnip.jump(-1)
+  --        else
+  --          fallback()
+  --        end
+  --      end, { "i", "s" }),
+  --    })
+  --  end,
+  --},
+
+  -- Automatically put the cursor on the Exporer tab
+  -- and close the picker when a file is selected.
+  -- https://www.reddit.com/r/neovim/comments/1ifv5go/help_with_snacks_explorer_config_i_have_read_the/
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            focus = "input",
+            auto_close = true,
+          },
+        },
+      },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
   },
 }
