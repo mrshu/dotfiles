e4b92aa9ddf1cd55a70ea1adb7767e6534deeeaf
mr.Shu <mr@shu.io>
2026-01-17 13:00:31 +0100

    chore(nvim): refine gutter spacing

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,31 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+
+-- Numbers & columns
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "auto:1-2"
+opt.statuscolumn = "%s %=%{v:relnum == 0 ? v:lnum : v:relnum} | "
+opt.numberwidth = 2
+opt.cursorline = true
+opt.colorcolumn = "100"
+
+-- Search & completion
+opt.ignorecase = true
+opt.smartcase = true
+opt.completeopt = { "menuone", "noselect" }
+
+-- Windows & scrolling
+opt.splitright = true
+opt.splitbelow = true
+opt.scrolloff = 6
+opt.sidescrolloff = 8
+
+-- Visual whitespace to make indentation consistent everywhere
+opt.list = true
+opt.listchars = {
+  tab = ">>",
+  trail = "~",
+  extends = ">",
+  precedes = "<",
+  nbsp = "+",
+}
